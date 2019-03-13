2a0ad081fc47feaeff56dcda95869ae8f26521e9
Masha Blair <mashablair@gmail.com>
2019-03-13 10:35:06 -0700

    Better way to bind event handlers by using arrow functions.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,14 +5,9 @@ class Counter extends Component {
     count: 0
   };
 
-  constructor() {
-    super();
-    this.handleIncrement = this.handleIncrement.bind(this);
-  }
-
-  handleIncrement() {
+  handleIncrement = () => {
     console.log("Increment Clicked", this);
-  }
+  };
 
   render() {
     return (
